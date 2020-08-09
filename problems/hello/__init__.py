b32114840f1765ccd27461b4864cf14775956fca
TheSTEMHub <69421415+TheSTEMHub@users.noreply.github.com>
2020-08-09 13:38:58 +0200

    Create __init__.py

---
+++ problems/hello/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """hello.cpp exists."""
+    check50.exists("hello.cpp")
+    
+@check50.check(exists)
+def Towfeq():
+    """responds to name Towfeq."""
+    check50.run("./hello").stdin("Towfeq").stdout("Towfeq").exit()
+
+@check50.check(exists)
+def brian():
+    """responds to name Veronica."""
+    check50.run("./hello").stdin("Brian").stdout("Brian").exit()
